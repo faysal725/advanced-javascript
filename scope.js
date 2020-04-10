9fa2da065fa24d2af45f3802080aac963970bc44
faysal725 <afaysal725@gmail.com>
2020-04-10 17:59:07 +0600

    scope idea

---
+++ scope.js
@@ -0,0 +1,17 @@
+let ghus = 20;
+
+function sum(first, second){
+    let result = first + second  + ghus;
+    // console.log(ghus);
+    if(result > 9){
+        let mood  = "happy";
+        
+        console.log(mood);
+    }
+    console.log(day);
+    let day = "friday";
+    
+    return result;
+   
+}
+const output = sum(3, 7);
