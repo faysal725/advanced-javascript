e4eb8ba64486a962e0a8c04fb1e786026bafeac6
faysal725 <afaysal725@gmail.com>
2020-04-10 06:27:45 +0600

    double vs tripple clear

---
+++ double-vs-tripple.js
@@ -0,0 +1,19 @@
+// true 
+// const first = 2;
+// const second = '2';
+// if(first == second)     {
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false");
+// }
+
+// false 
+const first = 2;
+const second = '2';
+if(first === second)     {
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
--- null-undefined.js
+++ null-undefined.js
@@ -1,23 +1,23 @@
 
 
 
+// function add(num1, num2){
+//     console.log(num1 +num2);
+//     return;
+// }
+
+
 function add(num1, num2){
-    console.log(num1 +num2);
-    return;
-}
-
-
-function add(num1, num2=6){
-    console.log(num1 + num2);
+    console.log(num1, num2);
 }
 const result = add(81);
 console.log(result);
 
-const premik = {name:"smart dude", phone:4564567};
-console.log(premik.gf);
+// const premik = {name:"smart dude", phone:4564567};
+// console.log(premik.gf);
 
-let fun = undefined;
-console.log(fun);
+// let fun = undefined;
+// console.log(fun);
 
-let ages = [2,4,8];
-console.log(ages[11]);
+// let ages = [2,4,8];
+// console.log(ages[11]);
