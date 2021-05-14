3d2a3059451b3c3d70d96ac03903af53a508240f
faysal725 <afaysal725@gmail.com>
2021-05-14 21:02:06 +0600

    null VS undefined clear for interview

---
+++ undefinedVsNull.js
@@ -0,0 +1,26 @@
+let fish;
+console.log(fish);
+
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+const result = add(13, 82);
+console.log(result);
+
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+const result = add(13);
+console.log(result);
+
+
+
+const Dredd = {name:"smart dude", phone: 50464046};
+console.log(Dredd.gf);
+
+
+let fun = undefined;
+console.log(fun);
+
+
